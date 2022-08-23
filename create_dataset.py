3ff78300b78c540d478dda3a99349fd0c631ca44
SKVP Owner <skvp.owner@gmail.com>
2022-08-23 23:48:41 +0300

    dataset creator initial

--- create_dataset.py
+++ create_dataset.py
@@ -1,4 +1,10 @@
+import sys
+import os
 import random
+import json
+import time
+from datetime import datetime
+from concurrent.futures import ProcessPoolExecutor
 
 import torch
 import torch.nn as nn
@@ -8,6 +14,7 @@ import torchvision.transforms as transforms
 import numpy as np
 
 
+DEVICE = 'cuda:0'
 MAX_THREADS = 4
 SEED = 20220819
 RECIPE = {
@@ -31,7 +38,7 @@ class Net(nn.Module):
 			kernel_size, width, stride = block['kernel_size'], block['width'], block['stride']
 			padding = kernel_size // 2
 			self.components.append(nn.Sequential(
-				nn.Conv2d(kernel_size, width, prev_tensor_width, stride, padding, bias = False),
+				nn.Conv2d(prev_tensor_width, width, kernel_size, stride, padding, bias = False),
 				nn.BatchNorm2d(width),
 				nn.ReLU()
 				)
@@ -67,41 +74,110 @@ class Net(nn.Module):
 		
 		return x	
 
-def create_sample(scheme, device, recipe, seed):
+def cifar10_dataset_train():
 	transform = transforms.Compose(
-		[transforms.ToTensor(),
-		 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-	net = Net(scheme).to(device)
-	torch.manual_seed(seed)
-	random.seed(seed)
-	np.random.seed(seed)
-	trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-											download=True, transform=transform)
-	trainloader = torch.utils.data.DataLoader(trainset, batch_size=recipe['batch_size'],
-											  shuffle=True, num_workers=1)
-	criterion = nn.CrossEntropyLoss()
-	optimizer = optim.SGD(net.parameters(), lr = recipe['lr'], momentum = recipe['momentum'], weight_decay = recipe['weight_decay'])
-	physical_batch_size = recipe['physical_batch_size']
-	for epoch in range(recipe['max_epochs']):
-		running_loss = 0.0
-		num_samples = 0
-		for i, data in enumerate(trainloader, 0):
-			inputs, labels = data
-			optimizer.zero_grad()
-			for j in range(0, len(inputs), physical_batch_size):
-				# Allowing big SGD batches and allowing to split them for physical computation, before taking the step
-				from_index = j
-				to_index = min(j + physical_batch_size, len(inputs))
-				inputs_j = inputs[from_index:to_index].to(device)
-				labels_j = labels[from_index:to_index].to(device)
-				outputs = net(inputs_j)
-				loss = criterion(outputs, labels_j)
-				loss.backward()
-				running_loss += loss.item()
-				num_samples += to_index - from_index
-			optimizer.step()
+			[transforms.ToTensor(),
+			 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+	return torchvision.datasets.CIFAR10(root='./data', train=True,
+												download=True, transform=transform)
+
+def create_sample(scheme, device, recipe, seed, output_file):		
+	with open(output_file, 'w', buffering = 1) as f:
+		net = Net(scheme).to(device)
+		f.write(f'{datetime.now()} Network has been init on device {device}\n')
+		torch.manual_seed(seed)
+		random.seed(seed)
+		np.random.seed(seed)
+		# First process should run separately to download the dataset without race condition
+		trainset = cifar10_dataset_train()
+		trainloader = torch.utils.data.DataLoader(trainset, batch_size=recipe['batch_size'],
+												  shuffle=True, num_workers=1)
+		f.write(f'{datetime.now()} Training set is ready\n')
+		criterion = nn.CrossEntropyLoss()
+		optimizer = optim.SGD(net.parameters(), lr = recipe['lr'], momentum = recipe['momentum'], weight_decay = recipe['wd'])
+		physical_batch_size = recipe['physical_batch_size']
+		f.write(f'{datetime.now()} Ready to start training\n')
+		for epoch in range(recipe['max_epochs']):
+			f.write(f'{datetime.now()} Starting epoch {epoch+1} of {recipe["max_epochs"]}\n')
+			batch_losses = []
+			for i, data in enumerate(trainloader, 0):
+				f.write(f'{datetime.now()} Batch {i+1} of {len(trainloader)}\n')
+				inputs, labels = data
+				optimizer.zero_grad()
+				physical_batch_loss = 0.0
+				loss_denominator = 0
+				for j in range(0, len(inputs), physical_batch_size):
+					# Allowing big SGD batches and allowing to split them for physical computation, before taking the step
+					from_index = j
+					to_index = min(j + physical_batch_size, len(inputs))
+					inputs_j = inputs[from_index:to_index].to(device)
+					labels_j = labels[from_index:to_index].to(device)
+					outputs = net(inputs_j)
+					loss = criterion(outputs, labels_j)
+					loss.backward()
+					physical_batch_loss += (to_index - from_index) * loss.item()
+					loss_denominator += to_index - from_index
+				batch_losses.append(physical_batch_loss / loss_denominator)
+				optimizer.step()
+			f.write(f'{datetime.now()} Loss mean, median, min: {np.mean(batch_losses)}, {np.median(batch_losses)}, {np.min(batch_losses)}\n')
 
 
-def create_dataset(schemes, output_file, device = DEVICE, recipe = RECIPE, seed = SEED, max_threads = MAX_THREADS):
-	for scheme in schemes:
+def add_scheme_to_pool(index, scheme, device, recipe, seed, output_file, executor, futures):
+	if os.path.isfile(output_file):
+		print(f'Warning: Output file already existing for scheme {index+1}: {output_file}. Skipping scheme...')
+		
+		return
+	futures.append(executor.submit(create_sample, scheme, device, recipe, seed, output_file))
+
+def create_dataset(schemes, output_dir, device = DEVICE, recipe = RECIPE, seed = SEED, max_threads = MAX_THREADS, allow_existing_output_dir = False):
+	if os.path.isdir(output_dir):
+		if (not allow_existing_output_dir):
+			raise Exception(f'Specified output directory already exists: {output_dir}')
+	else:
+		os.mkdir(output_dir)
+	if len(schemes) == 0:
+		return
+	print(f'{datetime.now()} Making sure that the CIFAR10 dataset is ready')
+	# Pre initializing dataset to ensure that it is ready to use
+	cifar10_dataset_train()
+	scheme_to_output_file = lambda index : os.path.join(output_dir, f'scheme_{index+1}.txt')
+	futures = []
+	"""
+	print(f'{datetime.now()} Starting execution of first scheme')
+	with ProcessPoolExecutor(max_workers = 1) as executor:
+		output_file = scheme_to_output_file(0)
+		run_scheme_on_pool(0, schemes[0], device, recipe, seed, output_file, executor, futures)
+	while len(futures) > 0 and (not futures[0].done()):
+		# Waiting on first scheme to finish (it should take care of downloading the dataset)
+		time.sleep(1)
+	print(f'{datetime.now()} Finished execution of scheme 1')
+	if futures[0].exception is not None:
+		raise futures[0].exception()
+	"""
+	print(f'{datetime.now()} Starting pool execution of all schemes')
+	with ProcessPoolExecutor(max_workers = max_threads) as executor:
+		for i, scheme in enumerate(schemes):
+			output_file = scheme_to_output_file(i)
+			add_scheme_to_pool(i, scheme, device, recipe, seed, output_file, executor, futures)
+		count_num_finished = lambda : len([1 for future in futures if future.done()])
+		print(f'{datetime.now()} {len(futures)} jobs were added to pool', flush = True)
+		#num_finished = count_num_finished()
+		while True:
+			# Waiting on all schemes to finish		
+			curr_num_finished = count_num_finished()
+			#if curr_num_finished > num_finished:
+			print(f'{datetime.now()} {curr_num_finished} of {len(futures)} jobs finished', flush = True)
+			#	num_finished = curr_num_finished
+			if curr_num_finished == len(futures):
+				break
+			time.sleep(60)
+		
+if __name__ == '__main__':
+	schemes_file = sys.argv[1]
+	output_path = sys.argv[2]
 	
+	with open(schemes_file, 'r') as f:
+		schemes = json.load(f)
+	
+	create_dataset(schemes, output_path)
